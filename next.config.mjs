236769fd35ed4203d04d831be9d47aaf7c7a1047
Joy Poddar <joypoddar.sd@gmail.com>
2024-02-21 11:11:47 +0530

    feat: class sorting and import organizations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: true,
+};
 
 export default nextConfig;
