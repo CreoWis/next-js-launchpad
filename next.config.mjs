f66bf6c4241043ac42f3a98de552c2a40447ef3a
Chhakuli Zingare <chhakulizingare12@gmail.com>
2025-01-20 12:51:43 +0530

    feat: add next intl plugin wrapper in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: true,
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
