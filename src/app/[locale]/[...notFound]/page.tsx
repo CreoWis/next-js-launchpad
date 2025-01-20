41ba7d12f12b1da3ba43a73a2e1764a20a5e2b25
Chhakuli Zingare <chhakulizingare12@gmail.com>
2025-01-20 14:37:31 +0530

    fix: fix the typo

--- src/app/[locale]/[...notFound]/page.tsx
+++ src/app/[locale]/[...notFound]/page.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-gray-800">
       <h1 className="mb-4 text-6xl font-bold">{t('404')} </h1>
-      <p className="mb-4 text-xl"> {t('dosent_exist_text')} </p>
+      <p className="mb-4 text-xl"> {t(`dosen't_exist_text`)} </p>
       <Link
         href="/"
         className="mt-4 rounded-lg bg-blue-700 px-6 py-3 text-white transition duration-300 hover:bg-blue-800"
