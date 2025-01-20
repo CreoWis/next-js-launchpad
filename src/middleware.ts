814ba9278ec3bb773a1bde75264071d18a1de558
Chhakuli Zingare <chhakulizingare12@gmail.com>
2025-01-20 19:50:13 +0530

    refactor: refactor the matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,5 @@ import { routing } from './navigation';
 export const middleware = createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    '/',
-    '/(en)/:path*',
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|logos|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)',
-  ],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
